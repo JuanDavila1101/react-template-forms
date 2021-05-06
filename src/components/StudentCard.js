21495d53d6d71e8c8770c55b80d52e79f9feb757
JuanDavila1101 <tatojuan713@hotmail.com>
2021-05-05 19:22:22 -0500

    adding the Delete to the forms

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -7,20 +7,27 @@ import {
 } from 'reactstrap';
 import PropTypes from 'prop-types';
 import StudentForm from './StudentForm';
+import { deleteStudent } from '../helpers/data/studentData';
 
 const StudentCard = ({
+  firebaseKey,
   name,
   grade,
   teacher,
-  // handleClick
-  handleClick,
-}) => (
+  setStudents,
+}) => {
+  const handleClick = () => {
+    deleteStudent(firebaseKey).then((authorArray) => setStudents(authorArray));
+    // console.warn(firebaseKey);
+    console.warn(setStudents);
+  };
+
+  return (
   <Card body>
       <CardTitle tag="h5">{name}</CardTitle>
       <CardText>Grade: {grade}</CardText>
       <CardText>Teacher: {teacher}</CardText>
       <Button color="danger" onClick={handleClick}>Delete Student</Button>
-      <Button color="info" onClick={handleClick}>Delete Student</Button>
       {
         <StudentForm
           formTitle='Edit Student'
@@ -30,14 +37,15 @@ const StudentCard = ({
         />
       }
   </Card>
-);
+  );
+};
 
 StudentCard.propTypes = {
   firebaseKey: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   teacher: PropTypes.string.isRequired,
   grade: PropTypes.number.isRequired,
-  handleClick: PropTypes.func,
+  setStudents: PropTypes.func,
 };
 
 export default StudentCard;
